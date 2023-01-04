025190ef5dc129d6fe9f6718d0f6c78a0003329a
MYORT <114438985+MYORT@users.noreply.github.com>
2023-01-04 23:41:13 +0300

    FIRST MY WEBSITE FILES

---
+++ index.js
@@ -0,0 +1,274 @@
+
+$(document).ready(function(){
+  $(".c").fadeOut();
+  $(".css").fadeOut();
+  $(".js").fadeOut();
+  $(".jquery").fadeOut();
+  $(".html").fadeOut();
+  $(".reactjs").fadeOut();
+  $(".python").fadeOut();
+  $(".bootstrap").fadeOut();
+});
+
+
+$(document).ready(function(){
+  $(".yavuz,.yavuz4").mouseover(function(){
+    $(".yavuz,.yavuz4").fadeIn();
+      $(".yavuz,.yavuz4").animate({
+        opacity: '1',
+      },8000);
+    $(".c").fadeIn();
+      $(".c").animate({
+        top: '-100px',
+        opacity: '1',
+        height:'100px',
+        width:'100px'
+      },8000);
+    $(".css").fadeIn();
+      $(".css").animate({
+        left: '-20px',
+        opacity: '1',
+      },8000);
+    $(".js").fadeIn();
+      $(".js").animate({
+        bottom: "90px",
+        left: "10px",
+        opacity: "1",
+      },8000);
+    $(".jquery").fadeIn();
+      $(".jquery").animate({
+        right: "380px",
+        opacity: "1",
+      },8000);
+    $(".html").fadeIn();
+      $(".html").animate({
+        top: "-50px",
+        left: "10px",
+        opacity: "1",
+      },8000);
+    $(".reactjs").fadeIn();
+      $(".reactjs").animate({
+        top: "90px",
+        right: "420px",
+        opacity: "1",
+      },8000);
+    $(".python").fadeIn();
+      $(".python").animate({
+        right: "660px",
+        top: "120px",
+        opacity: '1',
+      },8000);
+    $(".bootstrap").fadeIn();
+      $(".bootstrap").animate({
+        top: "-90px",
+        right: "450px",
+        opacity: "1",
+      },8000); 
+  });
+});
+
+
+$(document).ready(function(){
+  $(".twitter").fadeOut();
+  $(".instagram").fadeOut();
+  $(".linkedin").fadeOut();
+  $(".mail").fadeOut();
+  $(".whatsapp").fadeOut();
+  $(".youtube").fadeOut();
+  $(".facebook").fadeOut();
+  $(".tiktok").fadeOut();
+});
+
+
+$(document).ready(function(){
+  $(".yavuz2").mouseover(function(){
+    $(".yavuz2").fadeIn();
+      $(".yavuz2").animate({
+        opacity: '1',
+      },2000);
+    $(".twitter").fadeIn();
+      $(".twitter").animate({
+        opacity: '1',
+        left: '-490px',
+        top: '-20px',
+      },3000);
+    $(".instagram").fadeIn();
+      $(".instagram").animate({
+        left: '-185px',
+        top: '-215px',
+        opacity: '1',
+      },3000);
+    $(".linkedin").fadeIn();
+      $(".linkedin").animate({
+        left: "100px",
+        top: "-85px",
+        opacity: "1",
+      },3000);
+    $(".mail").fadeIn();
+      $(".mail").animate({
+        left: "390px",
+        top: "-290px",
+        opacity: "1",
+      },3000);
+    $(".whatsapp").fadeIn();
+      $(".whatsapp").animate({
+        left: "-620px",
+        top: "-95px",
+        opacity: '1',
+      },3000);
+    $(".youtube").fadeIn();
+      $(".youtube").animate({
+        left: "-85px",
+        top: "-285px",
+        opacity: "1",
+      },3000);
+    $(".facebook").fadeIn();
+      $(".facebook").animate({
+        left: "670px",
+        top: "-240px",
+        opacity: "1",
+      },3000);
+    $(".tiktok").fadeIn();
+      $(".tiktok").animate({
+        left: "-440px",
+        top: "-285px",
+        opacity: "1",
+      },3000);
+  });     
+});
+
+
+$(document).ready(function(){
+  $(".tub").fadeOut();
+  $(".viyana").fadeOut();
+  $(".sabanci").fadeOut();
+  $(".aachen").fadeOut();
+});
+
+$(document).ready(function(){
+  $(".mezun").mouseover(function(){
+    $(".mezun").fadeIn();
+      $(".mezun").animate({
+        opacity: '1',
+      },4000);
+    $(".tub").fadeIn();
+      $(".tub").animate({
+        opacity: '1',
+      },8000);
+    $(".viyana").fadeIn();
+      $(".viyana").animate({
+        opacity: '1',
+      },8000);
+    $(".sabanci").fadeIn();
+      $(".sabanci").animate({
+        opacity: "1",
+      },8000);
+    $(".aachen").fadeIn();
+      $(".aachen").animate({
+        opacity: "1",
+      },8000);
+  });     
+});
+
+
+
+
+
+$(document).ready(function(){
+  $(".yavuzmini").mouseover(function(){
+    $(".yavuzmini").animate({
+      opacity: '1',
+    });
+    $(".earth00").animate({
+      left: "-700px",
+      top: "-440px",
+      opacity: "1",
+    },8000);
+    $(".earth01").animate({
+        left: "720px",
+        top: "70px",
+        opacity: "1",
+    },8000);
+    $(".earth02").animate({
+        left: "170px",
+        top: "-30px",
+        opacity: "1",
+    },8000);
+    $(".earth03").animate({
+        left: "1020px",
+        top: "-330px",
+        opacity: "1",
+    },8000);
+    $(".earth10").animate({
+        left: "408px",
+        top: "210px",
+        opacity: "1",
+    },8000);
+    $(".earth11").animate({
+        left: "-152px",
+        top: "110px",
+        opacity: "1",
+    },8000); 
+    $(".earth12").animate({
+        left: "598px",
+        top: "-260px",
+        opacity: "1",
+    },8000); 
+    $(".earth13").animate({
+        left: "-252px",
+        top: "-220px",
+        opacity: "1",
+    },8000);
+    $(".earth20").animate({
+        left: "97px",
+        top: "260px",
+        opacity: "1",
+    },8000);
+    $(".earth21").animate({
+        left: "147px",
+        top: "60px",
+        opacity: "1",
+    },8000);
+    $(".earth22").animate({
+        left: "97px",
+        top: "-40px",
+        opacity: "1",
+    },8000);
+    $(".earth23").animate({
+        left: "347px",
+        top: "18px",
+        opacity: "1",
+    },8000);
+    $(".earth30").animate({
+        left: "-603px",
+        top: "490px",
+        opacity: "1",
+    },8000);
+    $(".earth31").animate({
+        left: "-283px",
+        top: "150px",
+        opacity: "1",
+    },8000);
+    $(".earth32").animate({
+        left: "297px",
+        top: "10px",
+        opacity: "1",
+    },8000);
+    $(".earth33").animate({
+        left: "647px",
+        top: "208px",
+        opacity: "1",
+    },8000);    
+  });     
+});
+
+$(document).ready(function(){
+  $(".yavuzmini").mouseover(function(){
+    $(".earth").delay(9000).animate({
+      opacity: '0',
+    });
+    $(".world").delay(8950).animate({
+      opacity: '1',
+    });
+  });     
+});
